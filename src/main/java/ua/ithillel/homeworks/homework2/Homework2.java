173adadc016447f96090f3a7e70ad12fdacb2661
Violetta Churikova <churikovavioletta@gmail.com>
2022-06-15 23:14:55 +0300

    Added lectures2

--- src/main/java/ua/ithillel/homeworks/homework2/Homework2.java
+++ src/main/java/ua/ithillel/homeworks/homework2/Homework2.java
@@ -5,22 +5,8 @@ import java.util.Random;
 
 public class Homework2 {
     public static void main(String[] args) {
-        helloWorld();                            // task - 1
-
-
-        Random random = new Random();            // task - 2
-        int[] array = new int[10];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(10) + 10;
-        }
-        System.out.println(Arrays.toString(array));
-        double sum = 0;
-        for (int i : array) {
-            sum += i;
-        }
-        double average = sum / array.length;
-        System.out.println(average);
-
+        helloWorld();                             // task - 1
+        randomNumbers();                          // task - 2
 
         int[] arr = {2, 7, 11, 15};               // task - 3
         int x = 9;
@@ -33,14 +19,32 @@ public class Homework2 {
             if (i % 3 != 0 && i % 5 != 0) {
                 System.out.print(i + " ");
             } else {
-                if (i % 3 == 0)
+                if (i % 3 == 0 && i % 5 != 0)
                     System.out.print("Hello ");
-                if (i % 5 == 0)
+                if (i % 5 == 0 && i % 3 != 0)
                     System.out.print("World ");
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.print("HelloWorld ");
+                }
             }
         }
+        System.out.println(" ");
     }
+    public static void randomNumbers() {
+        Random random = new Random();
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10) + 11;
+        }
+        System.out.println(Arrays.toString(array));
+        double sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        double average = sum / array.length;
+        System.out.println(average);
 
+    }
     public static int[] showSumIndexNumbers(int[] numbers, int target) {
         for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
